f1529943c573f826588d7618fb34a1d2ac71dadb
frodehus <frode.hus@outlook.com>
2021-03-24 19:13:43 +0100

    authorizationlevel needs to be anonymous

--- SecureFunctions/SecureFunc.cs
+++ SecureFunctions/SecureFunc.cs
@@ -9,20 +9,26 @@ namespace SecureFunctions
     public static class SecureFunc
     {
         [Function("SecureFunc")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
             FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("SecureFunc");
             if (!executionContext.IsAuthenticated())
             {
+                logger.LogWarning("User is not authorized");
                 var resp = req.CreateResponse(HttpStatusCode.Unauthorized);
+                resp.Headers.Add("Content-Type", "text/plain; charset=utf-8");
                 resp.WriteString(executionContext.GetAuthenticationError());
                 return resp;
             }
 
             if (!executionContext.IsInRole("My.App.Role"))
             {
-                return req.CreateResponse(HttpStatusCode.Forbidden);
+                logger.LogWarning("User does not have correct role");
+                var resp = req.CreateResponse(HttpStatusCode.Forbidden);
+                resp.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+                resp.WriteString("User does not have correct roles assigned");
+                return resp;
             }
 
             var response = req.CreateResponse(HttpStatusCode.OK);
